5c9eb64346aec45852fbe0e37c53b3195cdc0eb4
Anita Solanki <anita2solanki4@gmail.com>
2017-08-25 17:52:07 +0530

    challenge-data-table code submit

--- report.jsx
+++ report.jsx
@@ -1,28 +1,29 @@
 var React = require('react')
 var ReactPivot = require('react-pivot')
 var createReactClass = require('create-react-class')
+
+module.exports = createReactClass({
+  render () {
+    return (
+      <ReactPivot compact='true' rows={rows} dimensions={dimensions}
+        reduce={reduce} calculations={calculations}
+        activeDimensions={['Date', 'Host']} defaultStyles='false'
+        csvTemplateFormat='false' />
+    )
+  }
+})
+
 var rows = require('./data.json')
 
 var dimensions = [
-  {value: 'date', title: 'Date'},
+  {value: 'date', title: 'Date', className: 'react-pivot-date'},
   {value: 'host', title: 'Host'}
 ]
 
 var reduce = function (row, memo) {
-  switch (row.type) {
-    case 'impression': {
-      memo.impressions = ++memo.impressions || 1
-      break
-    }
-    case 'display': {
-      memo.displays = ++memo.displays || 1
-      break
-    }
-    case 'load': {
-      memo.loads = ++memo.loads || 1
-      break
-    }
-  }
+  if (row.type === 'impression') memo.impressions = ++memo.impressions || 1
+  if (row.type === 'display') memo.displays = ++memo.displays || 1
+  if (row.type === 'load') memo.loads = ++memo.loads || 1
   return memo
 }
 
@@ -30,45 +31,41 @@ var calculations = [
   {
     title: 'Impression',
     value: 'impressions',
-    template: function (val, row) {
-      return val
+    template: function (val) {
+      return val.toString()
     }
   },
   {
     title: 'Loads',
     value: 'loads',
-    template: function (val, row) {
-      return val
+    template: function (val) {
+      return val.toString()
     }
   },
   {
     title: 'Displays',
     value: 'displays',
-    template: function (val, row) {
-      return val
+    template: function (val) {
+      return val.toString()
     }
   },
   {
     title: 'Load Rate',
-    value: 'loadrate',
-    template: function (val, row) {
-      return ((row.loads / row.impressions) * 100).toFixed(1) + '%'
+    value: function (row) {
+      return ((row.loads / row.impressions) * 100)
+    },
+    template: function (val) {
+      return val.toFixed(1) + '%'
     }
   },
   {
     title: 'Display Rate',
-    value: 'displayrate',
-    template: function (val, row) {
-      return ((row.displays / row.loads) * 100).toFixed(1) + '%'
-    }
+    value: function (row) {
+      return ((row.displays / row.loads) * 100)
+    },
+    template: function (val) {
+      return val.toFixed(1) + '%'
+    },
+    className: 'react-pivot-display-date'
   }
 ]
-
-module.exports = createReactClass({
-  render () {
-    console.log(rows)
-    return (
-      <ReactPivot compact='true' rows={rows} dimensions={dimensions} reduce={reduce} calculations={calculations} activeDimensions={['Date', 'Host']} defaultStyles='false' csvTemplateFormat='false' />
-    )
-  }
-})
